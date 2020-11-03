8d87bde12d0587ad260aadc003a9b70c10301f1e
ibragim939 <ibragimfai@gmail.com>
2020-11-03 13:19:28 +0300

    Second Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# herokugo
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,13 @@
-from app.main import app
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_view():
+    return "<h1>The void consumes</h1>"
+    
 
 if __name__ == "__main__":
     app.run()

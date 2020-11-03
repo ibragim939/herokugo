de8f7d33738af57c6c67ac63a3651647971cf945
ibragim939 <ibragimfai@gmail.com>
2020-11-03 15:52:31 +0300

    5 Commit

--- wsgi.py
+++ wsgi.py
@@ -4,40 +4,31 @@ import requests
 
 app = Flask(__name__)
 
-INSTANCE_URL = "https://api.maytapi.com/api"
-PRODUCT_ID = "de68250b-bd93-4558-afb6-92ba61dbc367"
-PHONE_ID = "6962"
-API_TOKEN = "5c7d320a-f411-41db-8720-9102d4cbfd65"
 
-
-@app.route("/")
+@app.route("/sms")
 def sms_reply():
 
-    try:
+    INSTANCE_URL = "https://api.maytapi.com/api"
+    PRODUCT_ID = "de68250b-bd93-4558-afb6-92ba61dbc367"
+    PHONE_ID = "6962"
+    API_TOKEN = "5c7d320a-f411-41db-8720-9102d4cbfd65"
 
-        json_data = request.get_json()
-        phone = json_data['user']['phone']
-        text = json_data['message']['text']
+    json_data = request.get_json()
+    phone = json_data['user']['phone']
+    text = json_data['message']['text']
 
-        url = INSTANCE_URL + "/" + PRODUCT_ID + "/" + PHONE_ID + "/sendMessage"
-        headers = {
-            "Content-Type": "application/json",
-            "x-maytapi-key": API_TOKEN,
-        }
+    url = INSTANCE_URL + "/" + PRODUCT_ID + "/" + PHONE_ID + "/sendMessage"
+    headers = {
+        "Content-Type": "application/json",
+        "x-maytapi-key": API_TOKEN,
+    }
 
-        body = {"type": "text", "message": text,
-                "to_number": phone}
+    body = {"type": "text", "message": text,
+            "to_number": phone}
 
-        requests.post(url, json=body, headers=headers)
+    requests.post(url, json=body, headers=headers)
 
-        return str(json_data)
-
-    except Exception as e:
-        if repr(e) == "KeyError('user',)":
-            print('that KeyError but its fine')
-        else:
-            print(repr(e))
-        return str("bad_resp")
+    return str(json_data)
 
 
 @app.route("/testy")

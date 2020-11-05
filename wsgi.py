54a97c0948bc17d3a4602d2eb5b0103d9c2d680d
ibragim939 <ibragimfai@gmail.com>
2020-11-05 11:19:40 +0300

    The New Commit2

--- wsgi.py
+++ wsgi.py
@@ -19,13 +19,30 @@ def sms_reply():
         phone = json_data['user']['phone']
         text = json_data['message']['text']
 
+        baseurl = "https://baza-gai.com.ua/nomer/" + text
+        head = {'Accept': 'application/json'}
+
         url = INSTANCE_URL + "/" + PRODUCT_ID + "/" + PHONE_ID + "/sendMessage"
         headers = {
             "Content-Type": "application/json",
             "x-maytapi-key": API_TOKEN,
         }
 
-        body = {"type": "text", "message": text,
+        req = requests.get(baseurl, headers=head)
+
+        dic = req.json()
+        digits = dic['digits']
+        region = dic['region']['name']
+        vendor = dic['vendor']
+        model = dic['model']
+        year = dic['year']
+        operations = dic['operations'][0]['notes']
+        reg_at = dic['operations'][0]['regAt']
+
+        finaldata = "данные по номеру {}: \nрегион: {}\nмарка: {} {}\nпараметры: {}\nгод выпуска: {}\nдата регистрации: {}".format(
+            digits, region, vendor, model, operations, year, reg_at)
+
+        body = {"type": "text", "message": finaldata,
                 "to_number": phone}
 
         requests.post(url, json=body, headers=headers)
@@ -38,26 +55,6 @@ def sms_reply():
         else:
             print(repr(e))
         return str("bad_resp")
-
-
-# @app.route("/testy")
-# def testy():
-
-#     json_data = request.get_json()
-
-#     url = "https://api.maytapi.com/api" + "/" + \
-#         "de68250b-bd93-4558-afb6-92ba61dbc367" + "/" + "6962" + "/sendMessage"
-
-#     headers = {
-#         "Content-Type": "application/json",
-#         "x-maytapi-key": "5c7d320a-f411-41db-8720-9102d4cbfd65",
-#     }
-
-#     body = {"type": "text", "message": "text",
-#             "to_number": "79785921959"}
-#     requests.post(url, json=body, headers=headers)
-
-#     return str(json_data)
 
 
 if __name__ == "__main__":
